002f6ff814879df5c3034ea0decdb16e962a071a
Patrick Stephens <pat@calyptia.com>
2022-03-22 14:38:42 +0000

    Add buil from source

--- run.sh
+++ run.sh
@@ -3,8 +3,13 @@ set -eux
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 IMAGE=${IMAGE:-fluent-bit-dynamic-query}
-# git clone -b sp_dynamic_queries https://github.com/fluent/fluent-bit.git
-# docker build ...
+
+# If we have no source then assume not built it yet - remove source to force a rebuild too
+if [[ ! -d fluent-bit-source ]]; then
+    docker rmi -f "$IMAGE"
+    git clone -b stream_processing_dynamic_query https://github.com/fluent/fluent-bit.git fluent-bit-source
+    DOCKER_BUILDKIT=1 docker build --target=production -t "$IMAGE" -f "$SCRIPT_DIR"/fluent-bit-source/dockerfiles/Dockerfile "$SCRIPT_DIR"/fluent-bit-source
+fi
 
 docker rm -f fluent-bit-tap
 docker run --name fluent-bit-tap --rm -it -p 2020:2020 -v "$SCRIPT_DIR"/fluent-bit:/fluent-bit/etc:ro -d "$IMAGE"
